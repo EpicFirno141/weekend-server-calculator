582e6c7ca9591254b79dff4b899df819a7fe4fdd
Kaelan Leahy <kaelan@leahyonline.com>
2023-02-24 17:13:30 -0600

    Made new calculator logic that calculates for any calculation sent to it. Can perform using correct PEMDAS order and submits to DOM answer.

--- server/server.js
+++ server/server.js
@@ -77,6 +77,49 @@ function calculate(calc) {
     }
 }
 
+// This is just something that I made after submitting because I was curious
+// to see if I could actually make a functional calculator by myself. The only thing
+// that I ended up copying was the regex for the split functions. I believe that
+// this function works correctly.
+function newCalculator(calculation) {
+    if(calculation.includes('*') === true) {
+        let multSplit = calculation.split(/[+-/]+/);
+        for(let num of multSplit) {
+            if(num.includes('*')) {
+                let multiply = num.split('*');
+                let initMult = 1;
+                for(let numbers of multiply) {
+                    initMult = initMult * Number(numbers);
+                }
+                calculation = calculation.replace(num, String(initMult));
+            }
+        }
+    }
+    if(calculation.includes('/') === true) {
+        let divSplit = calculation.split(/[+-]+/);
+        for(let num of divSplit) {
+            if(num.includes('/')) {
+                let divide = num.split('/');
+                let initDiv = divide[0];
+                for(let i = 1; i<divide.length; i++) {
+                    initDiv = initDiv / Number(divide[i]);
+                }
+                calculation = calculation.replace(num, String(initDiv));
+            }
+        }
+    }
+    if(calculation.includes('+') === true || calculation.includes('-') === true) {
+        let preAddition = calculation.replaceAll('-','+-');
+        let addition = preAddition.split('+');
+        let initAdd = 0;
+        for(let numbers of addition) {
+            initAdd = initAdd + Number(numbers);
+        }
+        calculation = calculation.replace(calculation, String(initAdd));
+    }       
+    result = {result: calculation};
+}
+
 app.get('/result', (req, res) => {
     res.send(result);
     console.log('Sent result');
@@ -92,7 +135,8 @@ app.post('/calc', (req, res) => {
     console.log(newCalc);
     calculations.push(newCalc);
     console.log(calculations);
-    calculate(newCalc.calculation);
+   //calculate(newCalc.calculation);
+    newCalculator(newCalc.calculation);
     res.sendStatus(201);
 });
 
