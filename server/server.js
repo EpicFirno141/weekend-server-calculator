83d14408362abca84d0c2dabf33cebf8c7e901a7
Kaelan Leahy <kaelan@leahyonline.com>
2023-02-24 17:52:51 -0600

    I add a LOT of comments explaining my calculator logic.

--- server/server.js
+++ server/server.js
@@ -9,113 +9,59 @@ app.use(express.json());
 let calculations = [];
 let result = {result: 0};
 
-// This is kind of a hunk of code... Does all the calculations, but loaded with conditionals
-// The reason being is that it checks operators with conditionals prior to calculating
-// It also checks for multiple operators within the same calculation and tells the server console if there are and then stops
-function calculate(calc) {
-    console.log('This is calc: ' + calc);
-    if(calc.indexOf("+") < 0) {
-        if(calc.indexOf("-") < 0) {
-            if(calc.indexOf("*") < 0) {
-                if(calc.indexOf("/") < 0) {
-                    console.log('No operations detected');
-                } 
-                else if(calc.indexOf("-") >= 0 || calc.indexOf("*") >= 0 || calc.indexOf("+") >= 0) {
-                    console.log('Too many operators detected.');
-                }
-                else {
-                    let divCalc = calc.split("/");
-                    console.log(divCalc);
-                    let divNum = divCalc[0];
-                    for(let i = 1; i<divCalc.length; i++) {
-                        divNum = divNum / Number(divCalc[i]);
-                    }
-                    console.log(divNum);
-                    result = {result: divNum};
-                }
-            } 
-            else if(calc.indexOf("-") >= 0 || calc.indexOf("+") >= 0 || calc.indexOf("/") >= 0) {
-                console.log('Too many operators detected.');
-            }
-            else {
-                let multCalc = calc.split("*");
-                console.log(multCalc);
-                let multNum = 1;
-                for(let num of multCalc) {
-                    multNum = multNum * Number(num);
-                }
-                console.log('The answer is: ' + multNum);
-                result = {result: multNum};
-            }
-        } 
-        else if(calc.indexOf("+") >= 0 || calc.indexOf("*") >= 0 || calc.indexOf("/") >= 0) {
-            console.log('Too many operators detected.');
-        }
-        else {
-            let subCalc = calc.split("-");
-            console.log(subCalc);
-            let subbedNum = subCalc[0];
-            for (let i = 1; i<subCalc.length; i++) {
-                subbedNum -= Number(subCalc[i]);
-            }
-            console.log('The answer is: ' + subbedNum);
-            result = {result: subbedNum};
-        }
-    }
-    else if(calc.indexOf("-") >= 0 || calc.indexOf("*") >= 0 || calc.indexOf("/") >= 0) {
-        console.log('Too many operators detected.');
-    }
-    else {
-        let addCalc = calc.split("+");
-        console.log(addCalc);
-        let addedNum = 0;
-        for (let num of addCalc) {
-            addedNum += Number(num);
-        }
-        console.log('The answer is: ' + addedNum);
-        result = {result: addedNum};
-    }
-}
-
 // This is just something that I made after submitting because I was curious
 // to see if I could actually make a functional calculator by myself. The only thing
 // that I ended up copying was the regex for the split functions. I believe that
 // this function works correctly.
 function newCalculator(calculation) {
+    // Looks through calculation string and checks if it has multiplication
     if(calculation.includes('*') === true) {
+        // Splits the string by parameters +, -, and / (leaving only the multiplication left)
         let multSplit = calculation.split(/[+-/]+/);
         for(let num of multSplit) {
+            // Goes through all items in multSplit and only takes the ones with multiplication
             if(num.includes('*')) {
                 let multiply = num.split('*');
+                // Does multiplication here
                 let initMult = 1;
                 for(let numbers of multiply) {
                     initMult = initMult * Number(numbers);
                 }
+                // Takes section of original calculation with multiplication and replaces it with the finished product
                 calculation = calculation.replace(num, String(initMult));
             }
         }
     }
+    // Looks through calculation string and checks if it has division
     if(calculation.includes('/') === true) {
+        // Splits the string by the parameters + and - (leaving only the division left)
         let divSplit = calculation.split(/[+-]+/);
         for(let num of divSplit) {
+            // Goes through all items in divSplit and only proceeds with the ones with division
             if(num.includes('/')) {
                 let divide = num.split('/');
+                // Does division here
                 let initDiv = divide[0];
                 for(let i = 1; i<divide.length; i++) {
                     initDiv = initDiv / Number(divide[i]);
                 }
+                // Replaces all division operations in calculation with the result of division.
                 calculation = calculation.replace(num, String(initDiv));
             }
         }
     }
+    // Checks if the calculation string has any + OR - operations
     if(calculation.includes('+') === true || calculation.includes('-') === true) {
+        // Replaces all instances of - with +- (Effectively making them negative numbers that are added now)
         let preAddition = calculation.replaceAll('-','+-');
         let addition = preAddition.split('+');
+        // Does addition here
         let initAdd = 0;
         for(let numbers of addition) {
             initAdd = initAdd + Number(numbers);
         }
-        calculation = calculation.replace(calculation, String(initAdd));
+        // Since we are at the bottom of the list of operators, I just make calculation the finished product.
+        calculation = String(initAdd);
     }       
     result = {result: calculation};
 }
@@ -132,10 +78,7 @@ app.get('/calc', (req, res) => {
 
 app.post('/calc', (req, res) => {
     let newCalc = req.body;
-    console.log(newCalc);
     calculations.push(newCalc);
-    console.log(calculations);
-   //calculate(newCalc.calculation);
     newCalculator(newCalc.calculation);
     res.sendStatus(201);
 });
