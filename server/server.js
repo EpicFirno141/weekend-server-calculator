925bce11846c9e48c66a64facdf1c03174881d27
Kaelan Leahy <kaelan@leahyonline.com>
2023-02-23 19:28:56 -0600

    Finished server side post request. Now calculations are submitted to the server.

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ const PORT = 5001;
 app.use(express.static('server/public'));
 app.use(express.json());
 
+let calculations = [];
+
+app.post('/calc', (req, res) => {
+    let newCalc = req.body;
+    calculations.push(newCalc);
+    console.log(calculations);
+    res.sendStatus(201);
+})
+
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
