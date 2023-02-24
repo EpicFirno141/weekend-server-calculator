2401c4edae07b011f8c39fa65e15bc7d37fa954a
Kaelan Leahy <kaelan@leahyonline.com>
2023-02-23 19:54:05 -0600

    Started making a calculation function in the server. It stops if it detects usage of multiple operators. Currently it can add, more than just 2 numbers additionally.

--- server/server.js
+++ server/server.js
@@ -8,10 +8,58 @@ app.use(express.json());
 
 let calculations = [];
 
+function calculate(calc) {
+    console.log('This is calc: ' + calc);
+    if(calc.indexOf("+") < 0) {
+        if(calc.indexOf("-") < 0) {
+            if(calc.indexOf("*") < 0) {
+                if(calc.indexOf("/") < 0) {
+                    console.log('No operations detected');
+                } 
+                else if(calc.indexOf("-") >= 0 || calc.indexOf("*") >= 0 || calc.indexOf("+") >= 0) {
+                    console.log('Too many operators detected.');
+                }
+                else {
+                    let divCalc = calc.split("/");
+                    console.log(divCalc);
+                }
+            } 
+            else if(calc.indexOf("-") >= 0 || calc.indexOf("+") >= 0 || calc.indexOf("/") >= 0) {
+                console.log('Too many operators detected.');
+            }
+            else {
+                let multCalc = calc.split("*");
+                console.log(multCalc);
+            }
+        } 
+        else if(calc.indexOf("+") >= 0 || calc.indexOf("*") >= 0 || calc.indexOf("/") >= 0) {
+            console.log('Too many operators detected.');
+        }
+        else {
+            let subCalc = calc.split("-");
+            console.log(subCalc);
+        }
+    }
+    else if(calc.indexOf("-") >= 0 || calc.indexOf("*") >= 0 || calc.indexOf("/") >= 0) {
+        console.log('Too many operators detected.');
+    }
+    else {
+        let addCalc = calc.split("+");
+        console.log(addCalc);
+        let addedNum = 0;
+        for (let num of addCalc) {
+            addedNum += Number(num);
+        }
+        console.log('The answer is: ' + addedNum);
+    }
+}
+
 app.post('/calc', (req, res) => {
     let newCalc = req.body;
+    console.log(newCalc);
     calculations.push(newCalc);
     console.log(calculations);
+    calculate(newCalc.calculation);
     res.sendStatus(201);
 })
 
